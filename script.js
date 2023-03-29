39ab9fe291dd4b6b5d7be6fb7586775ff2097ece
gulsahbayazit <bayazitgulsahbayazit@gmail.com>
2023-03-29 16:36:33 +0200

    Bankist-app project created, js & css file added and worked on

---
+++ script.js
@@ -0,0 +1,266 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+displayMovements(account1.movements);
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+    return username;
+  });
+};
+createUsernames(accounts);
+// const user = 'Steven Thomas Williams'; // stv
+// console.log(username);
+
+//Event Handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+  accounts.find(acc => acc.username === inputLoginUsername.value);
+  if (currentAccount?.pin === inputLoginPin.value) {
+    console.log('LOGIN');
+  }
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+// //Slice Method
+// // IT DOESNT CHANGE ORIGINAL ARRAY
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// console.log(arr.slice(2, 4));
+
+// //Splice Method
+// // IT CHANGE ORIGINAL ARRAY
+
+// arr.splice(-1);
+// console.log(arr);
+// //It deletes the last element
+
+// arr.splice(1, 2);
+// //It takes the first element and delete 2 after it
+// // a,b,c,d
+// // result is a, d
+
+// // Reverse
+// // It mutuates the original array
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'h', 'i', 'f', 'g'];
+// console.log(arr2.reverse();
+
+// // Concat
+// // it unifies the two array
+// const letters = arr.concat(arr2);
+// console.log([...arr, ...arr2]);
+// console.log(letters);
+
+// // Join
+// console.log(letters.join('-'));
+
+// // at method
+// const arr3 = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log('jonas'.at(-1));
+
+// // Looping Array : forEach
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+// // forEach is high order function than it requires call back function
+// //loop over the array and execute each iteration of this  call back function
+// // mov : current element, i : current index, arr: current array
+// // it is important call back funtion here
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+// // 0: function(200)
+// // 1: function(450)
+// // 2: function(400)
+// // ...
+
+// forEach with Maps and Sets
+// 'USD' is key, 'United States dollar' is value
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// //The Set is a one-dimensional array with unique keys, while the Map is a two-dimensional array with key-value pairs, where each key shall be unique.
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogs = dogsJulia.slice(1, -1).concat(dogsKate);
+//   console.log(dogs);
+
+//   dogs.forEach(function (dog, i) {
+//     if (dog > 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a pupy`);
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// Data Transformation: map, filter, reduce
+
+// const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+// const movementsUSD = movements.map (mov =>
+//  mov * eurToUsd );
+
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
+
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )(mov)}`
+// );
+// console.log(movementsDescriptions);
+
+// Computing Usernames
+
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   return acc + cur;
+// }, 0);
+
+// const balance = movements.reduce((acc, cur) =>
+//   return acc + cur, 0);
+// console.log(balance)
+
+// let balance2 = 0;
+// for (const mov of movements) balance2 += mov;
+// console.log(balance2);
+
+// The Magic of Chaining Methods
+
+// const eurToUsd = 1.1;
+// console.log(movements);
+
+// //PIPELINE
+// const totalDepositsUSD = movements
+//   .filter(mov => mov > 0)
+//   .map((mov, i, arr) => {
+//     return mov * eurToUsd;
+//   })
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDepositsUSD);
+
+// const firstWithdrawal = movements.find(mov => mov < 0);
+// console.log(movements);
+// console.log(firstWithdrawal);
+
+// console.log(accounts);
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+// console.log(account);
+
+//Login Method
